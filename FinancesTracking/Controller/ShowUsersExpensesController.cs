f13f83726d1b67de25690b9a2909217906ffa0e3
vitorhugoanjos <vitorhugoanjos@outlook.pt>
2013-07-17 20:23:25 +0100

    Show user's expenses implemented ref #9

---
+++ FinancesTracking/Controller/ShowUsersExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancesTracking.Model;
+using FinancesTracking.Persistence;
+
+namespace FinancesTracking.Controller
+{
+    public class ShowUsersExpensesController
+    {
+        public List<Expense> GetUserExpenses(User user)
+        {
+            return Factory.GetFactory().GetExpensesRepository().GetUserExpenses(user);
+        }
+    }
+}
---
+++ FinancesTracking/View/ShowUsersExpensesUI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancesTracking.Model;
+using FinancesTracking.Persistence;
+using FinancesTracking.Controller;
+
+namespace FinancesTracking.View
+{
+    public class ShowUsersExpensesUI
+    {
+        private ShowUsersExpensesController controller = new ShowUsersExpensesController();
+
+        private User GetUser()
+        {
+            Console.WriteLine("Choose an  user: ");
+
+            foreach (User _user in Factory.GetFactory().GetUsersRepository().GetUsers())
+            {
+                Console.WriteLine(_user.Name);
+            }
+
+            string name = Console.ReadLine();
+
+            return Factory.GetFactory().GetUsersRepository().GetUser(name);
+        }
+
+        private void Header()
+        {
+            Console.WriteLine(" << SHOW USER EXPENSES >> \n");
+        }
+
+        private void ShowExpensesList(List<Expense> expenses)
+        { 
+            foreach(Expense expense in expenses)
+                Console.WriteLine(expense);
+            
+        }
+
+        public void Run()
+        {
+            User user;
+            Header();
+
+            do
+            {
+                user = GetUser();
+
+            } while (user == null);
+
+            ShowExpensesList(controller.GetUserExpenses(user));
+
+        }
+    }
+}
