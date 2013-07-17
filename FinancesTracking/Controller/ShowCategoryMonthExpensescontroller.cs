f83f6513c4d5cd9a587c48bccfcadb1bed53cc8b
vitorhugoanjos <vitorhugoanjos@outlook.pt>
2013-07-17 23:06:03 +0100

    Show category's and month's expenses implemented ref #16

---
+++ FinancesTracking/Controller/ShowCategoryMonthExpensescontroller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancesTracking.Model;
+using FinancesTracking.Persistence;
+using FinancesTracking.Controller;
+
+namespace FinancesTracking.Controller
+{
+    public class ShowCategoryMonthExpensescontroller
+    {
+        public List<Expense> GetMonthExpenses(int month, Category category)
+        {
+            List<Expense> expenses = GetCategoryExpenses(category);
+
+            List<Expense> monthExpenses = new List<Expense>();
+
+            foreach (Expense expense in expenses)
+            {
+                if (expense.Date.Month == month)
+                    monthExpenses.Add(expense);
+            }
+
+            return monthExpenses;
+        }
+
+        private List<Expense> GetCategoryExpenses(Category category)
+        {
+            List<Expense> expenses = Factory.GetFactory().GetExpensesRepository().GetAllExpenses();
+            List<Expense> filtered = new List<Expense>();
+
+            if (expenses.Count > 0)
+            {
+                foreach (Expense expense in expenses)
+                {
+                    if (expense.Category.Name.Equals(category.Name))
+                        filtered.Add(expense);
+                }
+            }
+
+            return filtered;
+        }
+    }
+}
---
+++ FinancesTracking/View/ShowCategoryMonthExpensesUI.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancesTracking.Model;
+using FinancesTracking.Persistence;
+using FinancesTracking.Controller;
+
+namespace FinancesTracking.View
+{
+    public class ShowCategoryMonthExpensesUI
+    {
+
+        private ShowCategoryMonthExpensescontroller controller = new ShowCategoryMonthExpensescontroller();
+
+        private void Header()
+        {
+            Console.WriteLine(" << SHOW MONTH/CATEGORY EXPENSES >> \n");
+        }
+
+        private void ShowExpensesList(List<Expense> expenses)
+        {
+            if (expenses.Count > 0)
+                foreach (Expense expense in expenses)
+                    Console.WriteLine(expense);
+            else
+                Console.WriteLine("There were no expenses registered in this month\n with this category");
+        }
+
+        private Category GetCategory()
+        {
+            Console.WriteLine("Choose a category: ");
+
+            foreach (Category _category in Factory.GetFactory().GetCategoriesRepository().GetAllCategories())
+            {
+                Console.WriteLine(_category.Name);
+            }
+
+            string name = Console.ReadLine();
+
+            return Factory.GetFactory().GetCategoriesRepository().GetCategory(name);
+        }
+
+        private int GetMonth()
+        {
+            int month;
+            Console.WriteLine("Which month do you want to consult?");
+            int.TryParse(Console.ReadLine(), out month);
+
+            return month;
+        }
+
+        public void Run()
+        {
+            Category category;
+
+            Header();
+
+            do
+            {
+                category = GetCategory();
+
+            } while (category == null);
+
+            List<Expense> expenses = controller.GetMonthExpenses(GetMonth(), category);
+            ShowExpensesList(expenses);
+
+        }
+
+    }
+}
