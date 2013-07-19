d996b54db8ee8d853544d1eccb328fcabfe253f3
vitorhugoanjos <vitorhugoanjos@outlook.pt>
2013-07-19 18:22:21 +0100

    Show all savings implemented ref #13

---
+++ FinancesTracking/View/ShowAllSavingsUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancesTracking.Controller;
+
+namespace FinancesTracking.View
+{
+    public class ShowAllSavingsUI : BaseUI
+    {
+        private ShowSavingsController controller = new ShowSavingsController();
+
+        public void Run()
+        {
+            Header("SHOW ALL SAVINGS");
+            PrintList(controller.ShowAllSavings(), "There are no savings registered");
+        }
+    }
+}
