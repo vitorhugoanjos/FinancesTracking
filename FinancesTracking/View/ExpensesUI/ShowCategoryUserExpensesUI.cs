c16e80bf68e6eb54649c32e2e5f5b0aa86265a3e
vitorhugoanjos <vitorhugoanjos@outlook.pt>
2013-07-19 18:41:48 +0100

    UI now extends BaseUI ref #17

--- FinancesTracking/View/ExpensesUI/ShowCategoryUserExpensesUI.cs
+++ FinancesTracking/View/ExpensesUI/ShowCategoryUserExpensesUI.cs
@@ -9,77 +9,22 @@ using FinancesTracking.Controller;
 
 namespace FinancesTracking.View
 {
-    public class ShowCategoryUserExpensesUI
+    public class ShowCategoryUserExpensesUI : BaseUI
     {
         private ShowCategoryUserExpensesController controller = new ShowCategoryUserExpensesController();
-
-        private void Header()
-        {
-            Console.WriteLine(" << SHOW EXPENSES BY USER AND CATEGORY  >> \n");
-        }
-
-        private User GetUser()
-        {
-            Console.WriteLine("Choose an  user: ");
-
-            foreach (User _user in Factory.GetFactory().GetUsersRepository().GetUsers())
-            {
-                Console.WriteLine(_user.Name);
-            }
-
-            string name = Console.ReadLine();
-
-            return Factory.GetFactory().GetUsersRepository().GetUser(name);
-        }
-
-        private Category GetCategory()
-        {
-            Console.WriteLine("Choose a category: ");
-
-            foreach (Category _category in Factory.GetFactory().GetCategoriesRepository().GetAllCategories())
-            {
-                Console.WriteLine(_category.Name);
-            }
-
-            string name = Console.ReadLine();
-
-            return Factory.GetFactory().GetCategoriesRepository().GetCategory(name);
-        }
-
-        private void ShowExpensesList(List<Expense> expenses)
-        {
-            if (expenses.Count > 0)
-
-                foreach (Expense expense in expenses)
-                    Console.WriteLine(expense);
-
-            else
-                Console.WriteLine("this user has no expenses in this category");
-
-        }
 
         public void Run()
         {
             Category category;
             User user;
-            Header();
 
-            do
-            {
-                user = GetUser();
-
-            } while (user == null);
-
-            do
-            {
-                category = GetCategory();
-
-            } while (category == null);
-
+            Header("SHOW EXPENSES BY USER AND CATEGORY");
+            user = GetUser();
+            category = GetCategory();
 
             List<Expense> expenses = controller.GetCategoryExpenses(category, user);
 
-            ShowExpensesList(expenses);
+            PrintList(expenses, "this user has no expenses in this category");
 
         }
     }
