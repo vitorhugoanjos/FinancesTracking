b1365a5086cef7cd8135e0fb1c443518ec69e3b0
vitorhugoanjos <vitorhugoanjos@outlook.pt>
2013-07-19 18:44:37 +0100

    UI now extends BaseUI ref #9

--- FinancesTracking/View/ExpensesUI/ShowUsersExpensesUI.cs
+++ FinancesTracking/View/ExpensesUI/ShowUsersExpensesUI.cs
@@ -9,53 +9,17 @@ using FinancesTracking.Controller;
 
 namespace FinancesTracking.View
 {
-    public class ShowUsersExpensesUI
+    public class ShowUsersExpensesUI : BaseUI
     {
         private ShowUsersExpensesController controller = new ShowUsersExpensesController();
-
-        private User GetUser()
-        {
-            Console.WriteLine("Choose an  user: ");
-
-            foreach (User _user in Factory.GetFactory().GetUsersRepository().GetUsers())
-            {
-                Console.WriteLine(_user.Name);
-            }
-
-            string name = Console.ReadLine();
-
-            return Factory.GetFactory().GetUsersRepository().GetUser(name);
-        }
-
-        private void Header()
-        {
-            Console.WriteLine(" << SHOW USER EXPENSES >> \n");
-        }
-
-        private void ShowExpensesList(List<Expense> expenses)
-        {
-            if (expenses.Count > 0)
-
-                foreach (Expense expense in expenses)
-                    Console.WriteLine(expense);
-
-            else
-                Console.WriteLine("This user has no expenses");
-            
-        }
 
         public void Run()
         {
             User user;
-            Header();
+            Header("SHOW USER EXPENSES");
+            user = GetUser();
 
-            do
-            {
-                user = GetUser();
-
-            } while (user == null);
-
-            ShowExpensesList(controller.GetUserExpenses(user));
+            PrintList(controller.GetUserExpenses(user), "This user has no expense");
 
         }
     }
